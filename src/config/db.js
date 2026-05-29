97f7fd049d57505c8c5a7160b97c6ed1b0711ed3
sinsin13 <adsnh13@gmail.com>
2026-05-30 03:27:42 +0530

    initialize backend with Express, MongoDB connection, and basic server setup

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("server is connected to DB");
+    })
+    .catch(err=>{
+        console.log("error connecting to DB", err.message);
+        process.exit(1)
+    })
+}
+
+module.exports = connectToDB
+
