cbf39e72bdb7d0603e801607896d2baea6ab056b
sinsin13 <adsnh13@gmail.com>
2026-06-05 16:55:35 +0530

    add jsonwebtoken for user authentication and update registration logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,31 @@
-const userModel = require("../models/user.model.js")
-
-
-
+const userModel = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
 // POST : /api/auth/register
 
-function userRegiseration(req, res){
-    const {email, password, name} = req.body
-}
+async function userRegiseration(req, res) {
+  const { email, password, name } = req.body;
 
+  const isExists = await userModel.findOne({ email: email });
+
+  if (isExists) {
+    return res
+      .status(422)
+      .json({ message: "User already exists", status: "failed" });
+  }
+
+  const user = await userModel.create({
+    email,
+    password,
+    name,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+  
+  
+}
 
 module.exports = {
-    userRegiseration
-}
+  userRegiseration,
+};
